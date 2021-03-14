b8643975ca631ea9ca3aa963908576ccd6274a24
kaz-yamada <kaz.yamada@outlook.com>
2021-03-14 13:03:46 +1100

    Add project files.

---
+++ LensPokerGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LensPokerGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
